ba356721c8c2a5849b820b36490180beb520e456
aoestate-chariots <68689932+aoestate-chariots@users.noreply.github.com>
2020-07-23 14:46:04 +0100

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose')
 const adminSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
     email:{
         type: String,
         required: true
